15524d800a30496e84ea6e1464c52e7a7f16a83b
aboobakkerfahiz57-design <aboobakkerfahiz57@gmail.com>
2026-09-15 19:46:55 +0530

    Add Supabase configuration file

---
+++ admin/supabase-config.js
@@ -0,0 +1,5 @@
+// Wheel 4U - Supabase Configuration
+
+const SUPABASE_URL = "https://fcdhvkehnomcfvgjbhfq.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_PGThTuAaVLZl_3rF0yFRFw_64OVs9zE]";
