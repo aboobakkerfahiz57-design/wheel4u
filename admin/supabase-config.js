e44b424c309a533d35cab0eb4822b3d0a45fffcd
aboobakkerfahiz57-design <aboobakkerfahiz57@gmail.com>
2026-09-16 15:53:41 +0530

    Fix Supabase publishable key formatting

--- admin/supabase-config.js
+++ admin/supabase-config.js
@@ -2,4 +2,4 @@
 
 const SUPABASE_URL = "https://fcdhvkehnomcfvgjbhfq.supabase.co";
 
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_PGThTuAaVLZl_3rF0yFRFw_64OVs9zE]";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_PGThTuAaVLZl_3rF0yFRFw_64OVs9zE";
